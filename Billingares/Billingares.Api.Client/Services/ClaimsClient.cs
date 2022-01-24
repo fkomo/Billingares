25a6b3204a186f0d4cd191ed0c71b5f9537729f5
filip komorovsky <filip.komorovsky@gmail.com>
2022-01-24 23:25:03 +0100

    basic minimal webapi + client library

---
+++ Billingares/Billingares.Api.Client/Services/ClaimsClient.cs
@@ -0,0 +1,29 @@
+﻿
+using Billingares.Api.Client.Base;
+using Billingares.Base;
+
+namespace Billingares.Api.Client.Services
+{
+	public class ClaimsClient : ClientBase, IClaimsClient
+	{
+		public ClaimsClient(string baseUrl) : base(baseUrl)
+		{
+
+		}
+
+		public Task<IEnumerable<Claim>> List(string id)
+		{
+			return Get<IEnumerable<Claim>>($"api/claims/{ id }");
+		}
+
+		public Task<IEnumerable<Claim>> Update(string id, Claim[] items)
+		{
+			return Post<Claim[], IEnumerable<Claim>>($"api/claims/{ id }", items);
+		}
+
+		public Task<Claim> Add(string id, Claim item)
+		{
+			return Post<Claim, Claim>($"api/claim/{ id }", item);
+		}
+	}
+}
---
+++ Billingares/Billingares.WebApi/Repositories/ClaimsRepository.cs
@@ -0,0 +1,45 @@
+﻿using Billingares.Base;
+
+namespace Billingares.WebApi.Repositories
+{
+	class ClaimsRepository
+	{
+		private readonly Dictionary<string, List<Claim>> Repository = new Dictionary<string, List<Claim>>();
+
+		public ClaimsRepository()
+		{
+		}
+
+		public IEnumerable<Claim> List(string id)
+		{
+			if (Repository.TryGetValue(id, out List<Claim> claims))
+				return claims;
+
+			var emptyList = Array.Empty<Claim>().ToList();
+			Repository.Add(id, emptyList);
+			return emptyList;
+		}
+
+		public Claim Add(string id, Claim item)
+		{
+			if (Repository.TryGetValue(id, out List<Claim> claims))
+				claims.Add(item);
+
+			else
+				Repository.Add(id, new List<Claim>(new Claim[] { item }));
+
+			return item;
+		}
+
+		public IEnumerable<Claim> Update(string id, Claim[] items)
+		{
+			if (Repository.Remove(id, out List<Claim> _))
+			{
+				Repository.Add(id, items.ToList());
+				return items;
+			}
+
+			return Array.Empty<Claim>().ToList();
+		}
+	}
+}
