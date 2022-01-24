0509f14835300e55146947ff75c3c8a9f638331c
filip komorovsky <filip.komorovsky@gmail.com>
2022-01-24 20:16:18 +0100

    docker script update

--- Billingares/Billingares.App/build-and-run-docker.ps1
+++ Billingares/Billingares.App/build-and-run-docker.ps1
@@ -13,10 +13,10 @@ try
 	docker image prune -a -f
 
 	#rem build new docker image
-	docker build -f dockerfile-Billingares.App -t billingares.app-wasm-docker .
+	docker build -f dockerfile-Billingares.App -t billingares.app-docker .
 
 	#rem run new image on localhost:8088
-	docker run -d --name billingares.app -p 8088:80 billingares.app-wasm-docker
+	docker run -d --name billingares.app -p 8088:80 billingares.app-docker
 
 	# remove temporary files
 	Remove-Item .\dockerfile-Billingares.App -verbose
@@ -35,7 +35,7 @@ finally
 }
 
 # save image
-docker save -o billingares.app.dock.tar billingares.app-wasm-docker
+docker save -o billingares.app-docker.tar billingares.app-docker
 
 # load image
-#docker load -i billingares.app.dock.tar
+#docker load -i billingares.app-docker.tar
