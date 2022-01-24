c9151082662c40b9b1f779c0230f6f6ed177809a
filip komorovsky <filip.komorovsky@gmail.com>
2022-01-24 19:58:13 +0100

    docker file rename

--- Billingares/Billingares.App/build-and-run-docker.ps1
+++ Billingares/Billingares.App/build-and-run-docker.ps1
@@ -4,7 +4,7 @@ Set-Location -Path ".."
 try
 {
 	# copy config files to solution root (temporary)
-	Copy-Item Billingares.App\.dockerfile -Destination .\.dockerfile-Billingares.App -verbose
+	Copy-Item Billingares.App\dockerfile -Destination .\dockerfile-Billingares.App -verbose
 	Copy-Item Billingares.App\nginx.conf -Destination . -verbose
 
 	# stop&remove old docker image
@@ -13,13 +13,13 @@ try
 	docker image prune -a -f
 
 	#rem build new docker image
-	docker build -f .dockerfile-Billingares.App -t billingares.app-wasm-docker .
+	docker build -f dockerfile-Billingares.App -t billingares.app-wasm-docker .
 
 	#rem run new image on localhost:8088
 	docker run -d --name billingares.app -p 8088:80 billingares.app-wasm-docker
 
 	# remove temporary files
-	Remove-Item .\.dockerfile-Billingares.App -verbose
+	Remove-Item .\dockerfile-Billingares.App -verbose
 	Remove-Item .\nginx.conf -verbose
 
 	Write-Output "... Success!"
