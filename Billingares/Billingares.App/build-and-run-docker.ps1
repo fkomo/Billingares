bb848aab9eea4ebb595547ea8bff0b2eac13e2a6
fkomo <filip.komorovsky@gmail.com>
2022-01-24 20:20:01 +0100

    dev to webapi merge (#10)
    * blazor dev merge (#7)
    
    * gitignore fix
    
    * claims table column fix
    
    * blazor app merge (#9)
    
    * gitignore fix
    
    * claims table column fix
    
    * uf fixes and improvements
    
    * docker file rename
    
    * docker script update

--- Billingares/Billingares.App/build-and-run-docker.ps1
+++ Billingares/Billingares.App/build-and-run-docker.ps1
@@ -4,7 +4,7 @@ Set-Location -Path ".."
 try
 {
 	# copy config files to solution root (temporary)
-	Copy-Item Billingares.App\.dockerfile -Destination .\.dockerfile-Billingares.App -verbose
+	Copy-Item Billingares.App\dockerfile -Destination .\dockerfile-Billingares.App -verbose
 	Copy-Item Billingares.App\nginx.conf -Destination . -verbose
 
 	# stop&remove old docker image
@@ -13,13 +13,13 @@ try
 	docker image prune -a -f
 
 	#rem build new docker image
-	docker build -f .dockerfile-Billingares.App -t billingares.app-wasm-docker .
+	docker build -f dockerfile-Billingares.App -t billingares.app-docker .
 
 	#rem run new image on localhost:8088
-	docker run -d --name billingares.app -p 8088:80 billingares.app-wasm-docker
+	docker run -d --name billingares.app -p 8088:80 billingares.app-docker
 
 	# remove temporary files
-	Remove-Item .\.dockerfile-Billingares.App -verbose
+	Remove-Item .\dockerfile-Billingares.App -verbose
 	Remove-Item .\nginx.conf -verbose
 
 	Write-Output "... Success!"
@@ -35,7 +35,7 @@ finally
 }
 
 # save image
-docker save -o billingares.app.dock.tar billingares.app-wasm-docker
+docker save -o billingares.app-docker.tar billingares.app-docker
 
 # load image
-#docker load -i billingares.app.dock.tar
+#docker load -i billingares.app-docker.tar
