2d3938ec3db179a2671435719e1be1ea954874d8
fkomo <filip.komorovsky@gmail.com>
2022-01-24 23:29:12 +0100

    working claims web api (#11)
    * dev to webapi merge (#10)
    
    * blazor dev merge (#7)
    
    * gitignore fix
    
    * claims table column fix
    
    * blazor app merge (#9)
    
    * gitignore fix
    
    * claims table column fix
    
    * uf fixes and improvements
    
    * docker file rename
    
    * docker script update
    
    * basic minimal webapi + client library

--- Billingares/Billingares.WebApi/Program.cs
+++ Billingares/Billingares.WebApi/Program.cs
@@ -1,6 +1,12 @@
 using Billingares.Base;
+using Billingares.WebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddJsonConsole();
+
+builder.Services.AddSingleton<ClaimsRepository>();
 
 // Add services to the container.
 
@@ -10,13 +16,57 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/claims", () =>
+
+app.MapGet("/", () =>
 {
-	return new Claim[]
+	return "Billingares.Api";
+});
+
+app.MapGet("/api/claims/{id}", ([FromServices] ClaimsRepository repository, string id) =>
+{
+	try
 	{
-		new Claim("user0", 100.0M, "user1, user2, user3"),
-		new Claim("user1", 50.0M, "user0, user2"),
-	};
+		if (string.IsNullOrWhiteSpace(id))
+			return Results.BadRequest(nameof(id));
+
+		return Results.Ok(repository.List(id));
+	}
+	catch (global::System.Exception)
+	{
+		return Results.Problem();
+	}
+});
+
+app.MapPost("/api/claim/{id}", ([FromServices] ClaimsRepository repository, string id, Claim item) =>
+{
+	try
+	{
+		if (string.IsNullOrWhiteSpace(id))
+			return Results.BadRequest(nameof(id));
+
+		item = repository.Add(id, item);
+
+		return Results.Ok(item);
+	}
+	catch (global::System.Exception)
+	{
+		return Results.Problem();
+	}
+});
+
+app.MapPost("/api/claims/{id}", ([FromServices] ClaimsRepository repository, string id, Claim[] items) =>
+{
+	try
+	{
+		if (string.IsNullOrWhiteSpace(id))
+			return Results.BadRequest(nameof(id));
+
+		return Results.Created($"/claims/{ id }", repository.Update(id, items));
+	}
+	catch (global::System.Exception)
+	{
+		return Results.Problem();
+	}
 });
 
 app.Run();
